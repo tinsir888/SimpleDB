2ddfad6bc3a82300cb91e30ed0722c92d3463856
tinsir888 <1911406@mail.nankai.edu.cn>
2021-04-02 18:43:48 +0800

    Finish JoinPredicate.java in exercise 1 of Lab 2. All local tests passed!

--- src/java/simpledb/JoinPredicate.java
+++ src/java/simpledb/JoinPredicate.java
@@ -24,8 +24,14 @@ public class JoinPredicate implements Serializable {
      *            Predicate.Op.LESS_THAN_OR_EQ
      * @see Predicate
      */
+    private final int field1;
+    private final Predicate.Op op;
+    private final int field2;
     public JoinPredicate(int field1, Predicate.Op op, int field2) {
         // some code goes here
+        this.field1 = field1;
+        this.op = op;
+        this.field2 = field2;
     }
 
     /**
@@ -36,24 +42,24 @@ public class JoinPredicate implements Serializable {
      */
     public boolean filter(Tuple t1, Tuple t2) {
         // some code goes here
-        return false;
+        return t1.getField(this.field1).compare(this.op, t2.getField(this.field2));
     }
     
     public int getField1()
     {
         // some code goes here
-        return -1;
+        return this.field1;
     }
     
     public int getField2()
     {
         // some code goes here
-        return -1;
+        return this.field2;
     }
     
     public Predicate.Op getOperator()
     {
-        // some code goes here
-        return null;
+        // some code goes here;
+        return this.op;
     }
 }
